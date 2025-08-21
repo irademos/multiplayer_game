4e1001250eafb82b5a6395892a4a4de2aed14719
irademos <74277397+irademos@users.noreply.github.com>
2025-08-21 16:21:12 -0400

    Add character chooser in settings

--- characters/PlayerCharacter.js
+++ characters/PlayerCharacter.js
@@ -4,14 +4,15 @@ import { createPlayerModel } from "../models/playerModel.js";
 import * as THREE from "three";
 
 export class PlayerCharacter extends CharacterBase {
-  constructor(username) {
+  constructor(username, modelPath) {
     const { model, nameLabel } = createPlayerModel(
       THREE,
       username,
       ({ mixer, actions }) => {
         this.mixer = mixer;
         this.actions = actions;
-      }
+      },
+      modelPath
     );
     super(model);
     this.nameLabel = nameLabel;
